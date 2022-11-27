f452a717d79db26b9b5876ed055130eba54ae284
cneis <96124560+cneis@users.noreply.github.com>
2022-11-27 20:14:03 +0100

    link statt a href

--- components/introHeader.js
+++ components/introHeader.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function IntroHeader() {
     return (
@@ -9,7 +10,11 @@ export default function IntroHeader() {
                     <p className='text-lg mb-6'>Du brauchst eine Landing Page, Portfolio-Seite, einen Blog? <br />
                     Du brauchst Wordpress-Unterstützung, oder möchtest eine Website in html und css gebaut haben? <br />
                     Oder Du möchtest Dein Projekt performant in React umgesetzt haben?</p>
-                    <a href='/work' className='inline-block bg-myblue-100 text-white px-6 py-3 text-lg rounded drop-shadow-md hover:drop-shadow-xl duration-700 hover:scale-110'>Check my work</a>
+                    <Link href='/work'>
+                        <a className='inline-block bg-myblue-100 text-white px-6 py-3 text-lg rounded drop-shadow-md hover:drop-shadow-xl duration-700 hover:scale-110'>Check my work</a>
+                    </Link>
+
+
                 </div>
                 <span className='m-5 inline-block before:w-full before:aspect-square before:block before:absolute before:-left-1.5 before:top-1.5 before:bg-myblue-100 relative before:block before:absolute before:bg-red-400 relative z-0 before:rounded-full'>
                     <Image src='/chris_portrait.svg'
