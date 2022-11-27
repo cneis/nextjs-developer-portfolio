9e0e4b2c2191e12929f9e6a76644570ec35ce19b
cneis <96124560+cneis@users.noreply.github.com>
2022-11-27 20:04:09 +0100

    work bg blue

--- components/work.js
+++ components/work.js
@@ -9,7 +9,7 @@ export default function Work({item}) {
             <div>
                 <h3 className='text-2xl mb-2 font-medium'>{item.title}</h3>
                 <span className='text-gray-600 mb-4 block'>
-                    <time className='bg-blue-800 text-white px-5 py-1.5 mr-4 rounded-xl'>
+                    <time className='bg-myblue-100 text-white px-5 py-1.5 mr-4 rounded-xl'>
                         {item.year}
                     </time>
                     {item.category}
