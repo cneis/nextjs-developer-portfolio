e6863b1c66a2b8af39594cc27030e5a62a01c6e2
cneis <96124560+cneis@users.noreply.github.com>
2022-11-28 21:45:22 +0100

    link statt a in blogpost

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -33,8 +33,14 @@ export default function PostPage({ frontmatter, content }) {
                 <div className='prose mx-auto'>
                 <nav>
                     <ul className='flex -ml-10 align-center list-none'>
-                        <li><a href='/blog'> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" /></svg></a></li>
-                        <li><a href='/blog'>Zurück zur Liste</a></li>
+                        <li>
+                            <Link href='/blog'> 
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" /></svg>
+                                </div>
+                            </Link>
+                        </li>
+                        <li><Link href='/blog'>Zurück zur Liste</Link></li>
                     </ul>
                 </nav>
                     <h1>{frontmatter.title}</h1>
